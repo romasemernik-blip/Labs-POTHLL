5f5092506a87f8e34b0fc8cd3ff93bf9b75c5195
romasemernik-blip <romasemernik@gmail.com>
2026-09-05 19:52:20 +0300

    Add name of classes and atributes

--- lab 1.cpp
+++ lab 1.cpp
@@ -1,5 +1,25 @@
 #include<iostream>
 #include<string>
+
+class Halls 
+{
+    private:
+    int num;
+    int num_seat;
+    string name;
+    public:
+
+};
+class Session
+{
+    private:
+    string film;
+    class Halls;
+    string date;
+    int time_start;
+    public:
+
+};
 int main ()
 {
     return 0;

ff1479fbad08907dcca7fb476d35f6518ecc74d6
romasemernik-blip <romasemernik@gmail.com>
2026-09-05 20:19:58 +0300

    Add In in classes and make constructors for In

--- lab 1.cpp
+++ lab 1.cpp
@@ -3,6 +3,10 @@
 
 class Halls 
 {
+    Halls (int num, int num_seat, string name)
+    {
+        In(num, num_seat, name);
+    }
     private:
     int num;
     int num_seat;
@@ -14,9 +18,19 @@ class Halls
       cout << "Number of Hall: " << num << endl;
       cout << "Number of seats: " << num_seat << endl;
     }
+    void In (int num, int num_seat, string name)
+    {
+        this->num = num;
+        this->num_seat = num_seat;
+        this->name = name;
+    }
 };
 class Session
 {
+    Session(string film, string date, int time_start)
+    {
+        In(film, date, time_start);
+    }
     private:
     string film;
     class Halls;
@@ -29,6 +43,12 @@ class Session
       cout << "Date of session: " << date << endl;
       cout << "Session start time: " << time_start << endl;
     }  
+     void In (string film, string date, int time_start)
+    {
+        this->film = film;
+        this->date = date;
+        this->time_start = time_start;
+    }
 
 };
 int main ()

68f4fb202b1975e102556b1977e491c6bc263ae8
romasemernik-blip <romasemernik@gmail.com>
2026-09-05 20:44:01 +0300

    Include  In and Out for different part of classes

--- lab 1.cpp
+++ lab 1.cpp
@@ -24,6 +24,30 @@ class Halls
         this->num_seat = num_seat;
         this->name = name;
     }
+    void In_num (int num)
+    {
+         this->num = num;
+    }
+    void In_num_seat (int num_seat)
+    {
+         this->num_seat = num_seat;
+    }
+    void In_name (string name)
+    {
+         this->name = name;
+    }
+    void Out_num ()
+    {
+         cout << "Number of Hall: " << num << endl;
+    }   
+     void Out_num_seat ()
+    {
+         cout << "Name of Hall: " << name << endl;
+    }   
+     void Out_name ()
+    {
+         cout << "Number of seats: " << num_seat << endl;
+    }    
 };
 class Session
 {
@@ -49,6 +73,31 @@ class Session
         this->date = date;
         this->time_start = time_start;
     }
+    void In_film (string film)
+    {
+         this->film = film;
+    }
+    void In_date (string date)
+    {
+         this->date = date;
+    }
+    void In_time_start (int time_start)
+    {
+         this->time_start = time_start;
+    }
+    void Out_film ()
+    {
+         cout << "Name of session film: " << film << endl;
+    }
+    void Out_date ()
+    {
+         cout << "Date of session: " << date << endl;
+    }  
+    void Out_time_start ()
+    {
+         cout << "Session start time: " << time_start << endl;
+    }     
+
 
 };
 int main ()
